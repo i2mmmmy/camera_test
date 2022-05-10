d6011a64fec8767cd4ca1e090682ccf84e5ae437
Your Name <you@example.com>
2022-05-09 22:53:16 -0700

    add switch to start/stop camera

--- camera_test/camera.py
+++ camera_test/camera.py
@@ -1,62 +1,77 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
 # encoding: utf-8
 import cv2
 import numpy as np
 import LineDetectionPlugin as LD
 
+
+import sys
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Float64
+from std_srvs.srv import SetBool, SetBoolResponse
 
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 outvideo = cv2.VideoWriter("out.avi",fourcc, 30.0, (640,480),True)
 
+cap = cv2.VideoCapture()
 
+def camera_switch_turn(req):
+    print("camera operate req received: %s" %(req.data))
+    if req.data:
+        global cap
+        cap = cv2.VideoCapture("./zoulang.mp4")
+    else:
+        cap.release()
 
+    if cap.isOpened():
+        print("camera now opened")
+    else:
+        print("camera now closed")
+    return SetBoolResponse(True, "ok")
 
 if __name__ == '__main__':
-    #cap = cv2.VideoCapture("./Data/WeChat_20220402195554.mp4")
-    #cap = cv2.VideoCapture("./Data/WeChat_20220402195624.mp4")
-    #cap = cv2.VideoCapture("./Data/WeChat_20220402195648.mp4")
-    rospy.init_node('talker', anonymous=True)
-    theta_pub = rospy.Publisher('bottomRight_X', Float64, queue_size=1)
-    #cap = cv2.VideoCapture(1)
-    # cap = cv2.VideoCapture("./zoulang.mp4")
-    cap = cv2.VideoCapture(4)
 
+    rospy.init_node('camera', anonymous=True)
+    theta_pub = rospy.Publisher('visual_track/bottomRight_X', Float64, queue_size=1)
+    cam_switch_srv = rospy.Service('visual_track/enable', SetBool, camera_switch_turn)
+    
     n = 1
     rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        while cap.isOpened(): 
+            print(n)
+            n = n+ 1
+            ret,frame = cap.read()
+            '''
+            print("main frame")
+            print(frame)
+            print("end read frame")
+            '''
+            if frame is None:
+                break
 
-    while cap.isOpened() and not rospy.is_shutdown():
-        print(n)
-        n = n+ 1
-        ret,frame = cap.read()
-        '''
-        print("main frame")
-        print(frame)
-        print("end read frame")
-        '''
-        if frame is None:
-            break
+            bottompointleft, slopleft, bottompointright, slopright, frame = LD.refineprocess(frame)
+            msg = Float64()
+            if len(bottompointright) == 0:
+                msg.data = -1.0
+                theta_pub.publish(msg)
+            else:
+                msg.data = bottompointright[0]
+                theta_pub.publish(msg)
+            
 
-        bottompointleft, slopleft, bottompointright, slopright, frame = LD.refineprocess(frame)
-        # print("bottommmmmmmmmmmmmmmmmmm:", type(bottompointleft))
-        msg = Float64()
-        if len(bottompointright) == 0:
-            msg.data = -1.0
-            theta_pub.publish(msg)
-        else:
-            msg.data = bottompointright[0]
-            theta_pub.publish(msg)
+            # 显示并保存结果
+            outvideo.write(frame)
+            cv2.imshow("result", frame)
+            cv2.waitKey(1)
+            rate.sleep()
         
-
-        # 显示并保存结果
-        outvideo.write(frame)
-        cv2.imshow("result", frame)
-        cv2.waitKey(1)
+        # 相机未开，等待服务
         rate.sleep()
 
+    rospy.spin()
     cap.release()
     outvideo.release()
